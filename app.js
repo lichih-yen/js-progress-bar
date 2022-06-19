7d8e89412187cd4773f37cbe30218f794230227d
Li-Chih Yen <lichih.yen@gmail.com>
2022-06-19 11:55:42 -0400

    updated the app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const progressBar = document.querySelector('#progress-bar')
+const section = document.querySelector('section')
+
+const animateProgressBar = () => {
+  let scrollDistance = -section.getBoundingClientRect().top
+  let progressWidth =
+    (scrollDistance / (section.getBoundingClientRect().height - document.documentElement.clientHeight)) * 100
+
+  let value = Math.floor(progressWidth)
+  progressBar.style.width = value + '%'
+
+  // make sure when we scroll too fast to the top, the bar will be 0
+  if (value < 0) {
+    progressBar.style.width = '0%'
+  }
+}
+
+window.addEventListener('scroll', animateProgressBar)
